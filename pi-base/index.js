b22894e28a652eaf82b0aad7a76901474a2e3677
Federico Zarantonello <fedezara2001@gmail.com>
2019-04-24 16:42:47 +0200

    pi program modified

--- pi-base/index.js
+++ pi-base/index.js
@@ -1,6 +1,20 @@
 var config = require('./config.js');
 var mqtt = require('mqtt');
 var GetMac = require('getmac');
+var Raspistill = require('node-raspistill').Raspistill;
+var crypto = require("crypto");
+var fs = require('fs');
+var Gpio = require('onoff').Gpio;
+var exec = require('child_process').exec;
+
+var pir = new Gpio(17, 'in', 'both');
+var raspistill = new Raspistill({
+    noFileSave: true,
+    encoding: 'jpg',
+    width: 640,
+    height: 480
+});
+
 
 var client = mqtt.connect({
     port: config.mqtt.port,
@@ -16,19 +30,73 @@ var client = mqtt.connect({
 
 client.on('connect', function() {
     client.subscribe('rpi');
-    GetMac.getMac(function(err, macAddress) {
+    GetMac.getMac(function(err, mac) {
         if (err) throw err;
-        client.publish('api-engine', macAddress);
+        macAddress = mac;
+        client.publish('api-engine', mac);
+        // startStreaming();
     });
+
 });
 
 client.on('message', function(topic, message) {
-    // message is Buffer
-    // console.log('Topic >> ', topic);
-    // console.log('Message >> ', message.toString());
+    message = message.toString();
     if (topic === 'rpi') {
-        console.log('API Engine Response >> ', message.toString());
+        console.log('API Engine Response >> ', message);
     } else {
         console.log('Unknown topic', topic);
     }
 });
+
+function startStreaming() {
+    raspistill
+        .timelapse(100, 0, function(image) { // every 100ms ~~FOREVER~~
+            var data2Send = {
+                data: {
+                    image: image,
+                    id: crypto.randomBytes(8).toString("hex")
+                },
+                macAddress: macAddress
+            };
+
+            client.publish('image', JSON.stringify(data2Send));
+            console.log('[image]', 'published');
+        })
+        .then(function() {
+            console.log('Timelapse Ended')
+        })
+        .catch(function(err) {
+            console.log('Error', err);
+        });
+}
+
+var isRec = false;
+
+// keep watching for motion
+pir.watch(function(err, value) {
+    if (err) exit();
+    if (value == 1 && !isRec) {
+        console.log('Intruder detected');
+        console.log('capturing video.. ');
+        isRec = true;
+        var videoPath = __dirname + '/video.h264';
+        var file = fs.createWriteStream(videoPath);
+        var video_path = './video/video' + Date.now() + '.h264';
+        var cmd = 'raspivid -o ' + video_path + ' -t 5000';
+
+        exec(cmd, function(error, stdout, stderr) {
+            // output is in stdout
+            console.log('Video Saved @ : ', video_path);
+            require('./mailer').sendEmail(video_path, true, function(err, info) {
+                setTimeout(function() {
+                    // isRec = false;
+                }, 3000); // don't allow recording for 3 sec after
+            });
+        });
+    }
+});
+
+function exit() {
+    pir.unexport();
+    process.exit();
+}
