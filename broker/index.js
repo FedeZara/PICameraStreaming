9a6f791a621cfacf645ea6db2623cbc6f45bdb47
Federico Zarantonello <fedezara2001@gmail.com>
2019-04-24 16:46:58 +0200

    broker file changed

--- broker/index.js
+++ broker/index.js
@@ -1,6 +1,5 @@
 let SSL_KEY = __dirname + '/certs/key.pem';
 let SSL_CERT = __dirname + '/certs/certificate.pem';
-let MONGOURL = 'mongodb://admin:admin123@tpsitiot-shard-00-00-v09b3.mongodb.net:27017,tpsitiot-shard-00-01-v09b3.mongodb.net:27017,tpsitiot-shard-00-02-v09b3.mongodb.net:27017/test?ssl=true&replicaSet=tpsitiot-shard-0&authSource=admin&retryWrites=true';
 
 module.exports = {
     id: 'broker',
@@ -13,9 +12,5 @@ module.exports = {
     secure: {
         keyPath: SSL_KEY,
         certPath: SSL_CERT,
-    },
-    backend: {
-        type: 'mongodb',
-        url: MONGOURL
     }
 };
